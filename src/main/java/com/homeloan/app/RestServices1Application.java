38ac641ea17f00e6da7488ef12d64de230ed750d
jaykumarbamania <90679000+jaykumarbamania@users.noreply.github.com>
2022-04-11 10:05:06 +0530

    Base Code

---
+++ src/main/java/com/homeloan/app/RestServices1Application.java
@@ -0,0 +1,13 @@
+package com.homeloan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServices1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServices1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/homeloan/app/RestServices1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeloan.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServices1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
